d308f36f47ac8e1e0c3014556103ffd93b18800f
Jianhong Yin <jiyin@redhat.com>
2020-08-02 17:22:13 +0800

    add my 1st automation script for single-node cluster
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # ontap-simulator-in-kvm
 show how to install and configure ONTAP simulator in KVM automatically
+
+# verified host OS
+Fedora-32, Fedora-33
+
+# dependent packages install
+```
+#1. kiss-vm
+git clone https://github.com/tcler/kiss-vm-ns; sudo make -C kiss-vm-ns; sudo vm --prepare
+
+#2. vncdotool
+sudo yum install -y python-devel
+pip install vncdotool
+
+#3. gocr
+sudo yum install -y gocr
+
+#*4. if you are non-root user, open new terminal and continue
+```
+
+# download ONTAP simulator image and license file, and convert image file to qcow2
+```
+# download url: https://mysupport.netapp.com/site/tools/tool-eula/simulate-ontap
+# note: need log in to the NetApp Support Site athttp://mysupport-beta.netapp.com/ before download
+tar vxf vsim-netapp-DOT9.7-cm_nodar.ova
+for i in {1..4}; do qemu-img convert -f vmdk -O qcow2 vsim-NetAppDOT-simulate-disk${i}.vmdk vsim-NetAppDOT-simulate-disk${i}.qcow2; done
+```
+
+# change/customize your own configuration in automation script. e.g:
+```
+#!/bin/bash
+#configure ontap simulator 9.7 as single cluster
+
+##please change/cusotmize bellow default configration at first
+vmname=ontap-single
+password=fsqe2020
+cluster_name=fsqe-sn-01
+managementif_addr=10.66.12.229
+managementif_mask=255.255.254.0
+managementif_gateway=10.66.13.254
+cluster_managementif_addr=192.168.100.11
+cluster_managementif_mask=255.255.255.0
+cluster_managementif_gateway=192.168.100.1
+dns_domain=192.168.100.1
+dns_addr=192.168.100.1
+controller_located=raycom
+```
+
+# run the automation script. e.g: ontap-simulator-9.7-auto.sh
+```
+bash ontap-simulator-9.7-auto.sh
+```
---
+++ ontap-simulator-9.7-auto.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+#configure ontap simulator 9.7 as single cluster
+
+##please change/cusotmize bellow default configration at first
+vmname=ontap-single
+password=fsqe2020
+cluster_name=fsqe-sn-01
+managementif_addr=10.66.12.229
+managementif_mask=255.255.254.0
+managementif_gateway=10.66.13.254
+cluster_managementif_addr=192.168.100.11
+cluster_managementif_mask=255.255.255.0
+cluster_managementif_gateway=192.168.100.1
+dns_domain=192.168.100.1
+dns_addr=192.168.100.1
+controller_located=raycom
+
+vnc_screen_text() {
+	local _vncaddr=$1
+	vncdo -s ${_vncaddr} capture _screen.png
+	convert _screen.png  -threshold 30%  _screen2.png
+	gocr -i _screen2.png 2>/dev/null
+}
+
+ocrgrep() {
+	local pattern=$1
+	local ignored_charset=${2:-ifk}
+	pattern=$(sed "s,[${ignored_charset}],.,g" <<<"${pattern}")
+	grep -i "${pattern}"
+}
+
+vm -n $vmname ONTAP-simulator -i vsim-NetAppDOT-simulate-disk1.qcow2 \
+	--disk=vsim-NetAppDOT-simulate-disk{2..4}.qcow2,bus=ide \
+	--net=ontap-isolate,e1000  --net=ontap-isolate,e1000 --net-macvtap=-,e1000 --net-macvtap=-,e1000 \
+	--noauto --force --nocloud --osv freebsd11.2 --bus=ide --msize $((6*1024)) --cpus 2
+
+read vncaddr <<<"$(vm vnc $vmname)"
+vncaddr=${vncaddr/:/::}
+[[ -z "$vncaddr" ]] && {
+	echo "{WARN}: something is wrong, exit ..." >&2
+	exit 1
+}
+
+:; echo -e "\n\033[1;36m=>" waiting: Hit .Enter. to boot immediately prompt ..."\033[0m"
+while sleep 0.5; do vnc_screen_text $vncaddr | ocrgrep "Hit .Enter. to boot immediately" && break; done
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: login prompt ..."\033[0m"
+while sleep 5; do vnc_screen_text $vncaddr | ocrgrep ^login: && break; done
+vncdo -s ${vncaddr} type "admin"
+vncdo -s ${vncaddr} key enter
+sleep 2
+vncdo -s ${vncaddr} type "reboot"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: reboot confirm prompt ..."\033[0m"
+while sleep 5; do vnc_screen_text $vncaddr | ocrgrep "Are you sure you want to reboot node.*? {y|n}:" && break; done
+vncdo -s ${vncaddr} type "y"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: Hit .Enter. to boot immediately prompt ..."\033[0m"
+while sleep 5; do vnc_screen_text $vncaddr | ocrgrep "Hit .Enter. to boot immediately" && break; done
+vncdo -s ${vncaddr} key enter
+
+: <<'COMM'
+:; echo -e "\n\033[1;36m=>" waiting: Boot Menu ask prompt ..."\033[0m"
+while sleep 5; do vnc_screen_text $vncaddr | ocrgrep "Press Ctrl-C for Boot Menu." && break; done
+vncdo -s ${vncaddr} key ctrl-c
+COMM
+
+:; echo -e "\n\033[1;36m=>" waiting: '"Type yes to confirm and continue {yes}:" ...'"\033[0m"
+while sleep 10; do vnc_screen_text $vncaddr | ocrgrep "Type yes to confirm and continue {yes}:" && break; done
+vncdo -s ${vncaddr} type "yes"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: node management interface port prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the node management interface port" && break; done
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: node management interface ip address prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the node management interface .. address" && break; done
+vncdo -s ${vncaddr} type "$managementif_addr"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: node management interface ip netmask prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the node management interface netmask" && break; done
+vncdo -s ${vncaddr} type "$managementif_mask"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: node management interface gateway prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the node management interface default gateway" && break; done
+vncdo -s ${vncaddr} type "$managementif_gateway"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: cluster setup prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "cluster setup using the command line" && break; done
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: create a new cluster or join an existing cluster? prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "create a new cluster or join an" && break; done
+vncdo -s ${vncaddr} type "create"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: as a single node cluster? prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "used as a single node cluster?" && break; done
+vncdo -s ${vncaddr} type "yes"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: 'administrators(username "admin") password prompt ...'"\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "administrator.* password:" && break; done
+vncdo -s ${vncaddr} type "$password"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: password retype prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Retype the password:" && break; done
+vncdo -s ${vncaddr} type "$password"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: cluster name ask prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the cluster name:" && break; done
+vncdo -s ${vncaddr} type "$cluster_name"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: license key prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter an additional license key" && break; done
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: cluster management interface port prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the cluster management interface port" && break; done
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: cluster management interface ip address prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the cluster management interface .. address" && break; done
+vncdo -s ${vncaddr} type "$cluster_managementif_addr"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: cluster management interface ip netmask prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the cluster management interface netmask" && break; done
+vncdo -s ${vncaddr} type "$cluster_managementif_mask"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: cluster management interface gateway prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the cluster management interface default gateway" && break; done
+vncdo -s ${vncaddr} type "$cluster_managementif_gateway"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: DNS domain namesask prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the DNS domain names" && break; done
+vncdo -s ${vncaddr} type "$dns_domain"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: name server IP addresses ask prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "Enter the name server .. addresses" && break; done
+vncdo -s ${vncaddr} type "$dns_addr"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: where is the controller located prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "controller located" && break; done
+vncdo -s ${vncaddr} type "$controller_located"
+vncdo -s ${vncaddr} key enter
+
+:; echo -e "\n\033[1;36m=>" waiting: backup destination address prompt ..."\033[0m"
+while sleep 2; do vnc_screen_text $vncaddr | ocrgrep "backup destination address" && break; done
+vncdo -s ${vncaddr} key enter
+sleep 2
+
+:; echo -e "\n\033[1;36m------------------------------------------------------\033[0m"
+vnc_screen_text $vncaddr | GREP_COLORS='ms=01;36' grep --color .
+:; echo -e "\n\033[1;36m------------------------------------------------------\033[0m"
+
+:; echo -e "\n\033[1;36m=>" "now ssh(admin@$cluster_managementif_addr) is available, please complete other configurations in ssh session ...""\033[0m"
