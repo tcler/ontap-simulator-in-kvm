3193d7669bc8b9b205dccf608ce83d7ef96f23d4
Jianhong Yin <jiyin@redhat.com>
2020-09-10 14:56:44 +0800

    add utils/start_ntp_server.sh and move get_valid_ip_list.sh into utils
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

---
+++ utils/start_ntp_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LANG=C
+P=$0; [[ $0 = /* ]] && P=${0##*/}
+AT=("$@")
+switchroot() {
+	[[ $(id -u) != 0 ]] && {
+		echo -e "{NETNS:WARN} $P need root permission, switch to:\n  sudo $P ${AT[@]}" | GREP_COLORS='ms=1;30' grep --color=always . >&2
+		exec sudo $P "${AT[@]}"
+	}
+}
+switchroot;
+
+which chronyd &>/dev/null || yum install -y chrony >/dev/null
+systemctl enable chronyd
+systemctl start chronyd
