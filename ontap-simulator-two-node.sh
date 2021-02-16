6ceb13a98bbc1cc48826b0f9d0f4ce4706f892b7
Jianhong Yin <jiyin@redhat.com>
2021-02-16 23:12:17 +0800

    ontap-simulator-two-*: fix expect timeout
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- ontap-simulator-two-node.sh
+++ ontap-simulator-two-node.sh
@@ -948,7 +948,10 @@ expect -c "spawn ssh admin@$cluster_managementif_addr
 	}
 	expect {${cluster_name}::>} {
 		send \"kerberos interface enable -lif $LIF2_1_NAME -admin-username ${AD_ADMIN} -spn nfs/${NAS_SERVER_NAME}.${AD_DOMAIN}@${AD_REALM}\\r\"
-		expect {Password:} { send \"${AD_PASSWD}\\r\" }
+		expect {
+			{Password:} { send \"${AD_PASSWD}\\r\" }
+			{${cluster_name}::>} { send \"\\r\" }
+		}
 	}
 	expect {${cluster_name}::>} { send \"exit\\r\" }
 	expect eof
